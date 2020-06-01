7303f1a003b8e9485327ee7527be860e485bf53d
Thiago Gonçalves <goncalves.thiago@ctis.com.br>
2020-06-01 13:32:35 -0300

    Criação da estrutura base do projeto

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        {
+            "name": "Thiago Gonçalves"
+        }
+    ]);
+});
+
+app.listen(3333);
