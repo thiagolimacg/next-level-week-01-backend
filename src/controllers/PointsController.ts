2225202656f6de3739c31696a6601fd34ba758dc
Thiago Gonçalves <goncalves.thiago@ctis.com.br>
2020-06-02 23:22:59 -0300

    Criação do endpoint para listar um ponto de coleta por id

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -2,6 +2,22 @@ import { Request, Response } from 'express';
 import knex from '../database/connection'
 
 class PointsControlle {
+    
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+        const point = await knex('points').where('id', id).first();
+
+        if (!point) {
+            return response.status(400).json({ message: 'Point not found.' })            
+        }
+
+        const items = await knex('items')
+        .join('points_items', 'items.id', '=', 'points_items.item_id')
+        .where('points_items.point_id', id)
+        .select('items.title');
+
+        return response.json({ point, items });
+    }
 
     async create(request: Request, response: Response) {
         const {name, email, whatsapp, latitude, longitude, city, uf, items} = request.body;
@@ -30,5 +46,6 @@ class PointsControlle {
             ...point,
         });
     }
+
 }
 export default PointsControlle;
