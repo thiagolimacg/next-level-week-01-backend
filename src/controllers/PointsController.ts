f8c6b57f88c2d838c682085431e8311ab706c39f
Thiago Gonçalves <goncalves.thiago@ctis.com.br>
2020-06-02 23:02:42 -0300

    Criação da pasta controllers para separar as responsabilidades e reestruturação do código para atender o uso dos controllers

---
+++ src/controllers/PointsController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection'
+
+class PointsControlle {
+
+    async create(request: Request, response: Response) {
+        const {name, email, whatsapp, latitude, longitude, city, uf, items} = request.body;
+        
+        const trx = await knex.transaction();
+        
+        //full sintaxe, ex: name(propriedade): name(variável), email: email
+        //short sintaxe, é possível utilizar quando o nome da variável é igual ao nome da propriedade do objeto
+        const point = { image: 'image-fake', name, email, whatsapp, latitude, longitude, city, uf }
+    
+        const insertedIds = await trx('points').insert(point);
+    
+        const point_id = insertedIds[0];
+    
+        const pointItems = items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id,
+            }
+        });
+    
+        await trx('points_items').insert(pointItems);
+    
+        return response.json({
+            id: point_id,
+            ...point,
+        });
+    }
+}
+export default PointsControlle;
--- src/routes.ts
+++ src/routes.ts
@@ -1,43 +1,14 @@
 import express, { request, response, json } from 'express';
-import knex from './database/connection'
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`, 
-        }
-    });
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
-routes.post('/points', async (request, response) => {
-    const {name, email, whatsapp, latitude, longitude, city, uf, items} = request.body;
-    
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        //full sintaxe, ex: name(propriedade): name(variável), email: email
-        //short sintaxe, é possível utilizar quando o nome da variável é igual ao nome da propriedade do objeto
-        image: 'image-fake', name, email, whatsapp, latitude, longitude, city, uf
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    });
-
-    await trx('points_items').insert(pointItems);
-
-    return response.json({success: true});
-})
+routes.post('/points', pointsController.create)
 
 export default routes;
